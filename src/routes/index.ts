2801a03d0222ec62bedb6eceb182646399112027
Hassan Ansari <hassanansari222@gmail.com>
2023-08-07 00:54:47 +0530

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,41 +9,16 @@ import crypto from "crypto";
 import { url } from "inspector";
 
 const defaultRouter: FastifyPluginCallback = (fastify, options, done) => {
+  fastify.get("/", async (request, reply) => {
+    return { hello: "world" };
+  });
+
   fastify.register(projectRoute, { prefix: "/projects" });
   fastify.register(experienceRoute, { prefix: "/experiences" });
   fastify.register(contactRoute, { prefix: "/contact" });
   fastify.register(resumeRoute, { prefix: "/resume" });
   fastify.register(techStackRouter, { prefix: "/techStacks" });
 
-  fastify.post("/pay", async (request, reply) => {
-    var hash = crypto.createHash("sha256");
-    const json = JSON.stringify(request.body);
-    const encoded = Buffer.from(json).toString("base64");
-    const saltKey = "cc2f75ad-01c2-4417-92f8-32964ce8d12d";
-    const saltIndex = "1";
-    const dataToEncode = encoded + "/pg/v1/pay" + saltKey;
-    hash.update(dataToEncode);
-    const hashValue = hash.digest("hex");
-    const headerVerification = hashValue + "###" + saltIndex;
-    const headers = {
-      accept: "application/json",
-      "Content-Type": "application/json",
-      "X-VERIFY": headerVerification,
-    };
-    try {
-      const response = await axios.post(
-        "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay",
-        request.body,
-        {
-          headers: headers,
-        }
-      );
-      reply.send(response);
-    } catch (e) {
-      console.log(e);
-      reply.send(e);
-    }
-  });
   done();
 };
 
