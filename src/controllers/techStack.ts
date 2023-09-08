07d74e431192bbfafcee6e1b5ceaa133667b48e5
Hassan Ansari <hassanansari222@gmail.com>
2023-09-08 19:40:35 +0530

    Fix

--- src/controllers/techStack.ts
+++ src/controllers/techStack.ts
@@ -50,8 +50,8 @@ async function getTechStackById(id: string): Promise<TechStack | null> {
 
 async function updateTechStack(id: string, body: any): Promise<TechStack> {
   try {
-    const image = body.image[0].data;
-/*     if (image) {
+    // const image = body.image[0].data;
+    /*     if (image) {
       const upload: UploadApiResponse = await new Promise((resolve, reject) => {
         v2.uploader
           .upload_stream({ folder: "techStacks" }, (error, result) => {
@@ -70,7 +70,11 @@ async function updateTechStack(id: string, body: any): Promise<TechStack> {
       where: {
         id: id,
       },
-      data: body,
+      data: {
+        color: body.color,
+        name: body.name,
+        link: body.link,
+      },
     });
     return techStack;
   } catch (error) {
